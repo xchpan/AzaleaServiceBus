9d834de3e1585032ce0b4c616207a14e0cfb0049
xchpan <xchpan@gmail.com>
2013-11-04 01:06:18 -0800

    Initialize the sample temperature publisher.

---
+++ Source/SamplePublisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+using System.Threading;
+using System.Xml;
+using xpan.AzaleaServiceBus.ServiceContracts;
+
+namespace xpan.AzaliaServiceBus.SamplePublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exist the temprature publisher...");
+
+            var category = new Guid();
+            var id = new Guid();
+            using (var factory = new ChannelFactory<IPublish>("publisher"))
+            {
+                var channel = factory.CreateChannel();
+                var registration = channel.Register(new RegistrationRequest(typeof (Temperature), category, id, string.Empty));
+                do
+                {
+                    channel.Publish(registration.Id, new XmlElement());
+                    Thread.Sleep(TimeSpan.FromMilliseconds(500));
+                } while (!Console.KeyAvailable);
+            }
+        }
+    }
+}
---
+++ Source/SamplePublisher/Temperature.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Serialization;
+
+namespace xpan.AzaliaServiceBus.SamplePublisher
+{
+    public class Temperature
+    {
+        public string Unit { get; set; }
+        public double Value { get; set; }
+    }
+}
