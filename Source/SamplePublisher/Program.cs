0963c26b274bd358ab60b7fcdcfcb64128b4b1b2
xchpan <xchpan@gmail.com>
2013-11-24 00:44:57 -0800

    Create sample publisher

--- Source/SamplePublisher/Program.cs
+++ Source/SamplePublisher/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Configuration;
 using System.Threading;
 using System.Xml;
+using System.Xml.Serialization;
 using xpan.AzaleaServiceBus.ServiceContracts;
 
 namespace xpan.AzaliaServiceBus.SamplePublisher
@@ -20,9 +24,35 @@ namespace xpan.AzaliaServiceBus.SamplePublisher
                 var registration = channel.Register(new RegistrationRequest(typeof (Temperature), category, id, string.Empty));
                 do
                 {
-                    channel.Publish(registration.Id, new XmlElement());
+                    channel.Publish(registration.Id, ToXmlElement(getNewTemperature()));
                     Thread.Sleep(TimeSpan.FromMilliseconds(500));
                 } while (!Console.KeyAvailable);
+            }
+        }
+
+        private static int index = 0;
+        private static int max = 200;
+
+        private static Temperature getNewTemperature()
+        {
+            var x = ++index%max - max/2;
+            return new Temperature()
+            {
+                Unit = "F",
+                Value = 70 + x*0.08
+            };
+        }
+
+        private static XmlElement ToXmlElement(object obj)
+        {
+            var doc = new XmlDocument();
+            var serializer = new XmlSerializer(obj.GetType());
+            using (var stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, obj);
+                stream.Position = 0;
+                doc.Load(stream);
+                return doc.DocumentElement;
             }
         }
     }
